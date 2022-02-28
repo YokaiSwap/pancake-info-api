e0f68d67b9a7d9a4c40a59d9f20858a2e8c4dc60
TheEyeOfWadjet <81730561+TheEyeOfWadjet@users.noreply.github.com>
2022-02-28 16:30:41 +0800

    feat: Match api & schema

--- utils/apollo/client.ts
+++ utils/apollo/client.ts
@@ -4,7 +4,7 @@ import fetch from "cross-fetch";
 export const client = new ApolloClient({
   link: new HttpLink({
     fetch,
-    uri: "https://api.bscgraph.org/subgraphs/name/pancakeswap/exchange-lite",
+    uri: "https://www.yokaiswap.com/subgraphs/name/yokaiswap/exchange",
   }),
   cache: new InMemoryCache(),
 });
